63493d5b5ab15b630247f335ca33f7e2af914e1b
kalvintn <63025530+kalvintn@users.noreply.github.com>
2025-11-11 16:56:00 -0500

    Finish CSS styling.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 let calculator = document.querySelector(".calculator");
-
-
-let grid_values = ["0", "+", "-", "*", "/", "7", "8", "9", "AC", "4", "5", "6", "&#8592;", "1", "2", "3", "=", "0", "."];
+let grid_values = ["0", "&#x2b;", "&#8722;", "&#215;", "&#247;", "7", "8", "9", "AC", "4", "5", "6", "&#8592;", "1", "2", "3", "=", "0", "."];
 let grid_classes = ["display", "add", "subtract", "multiply", "divide", "seven", "eight", "nine", "clear", "four", "five", "six", "backspace", "one", "two", "three", "equals", "zero", "decimal"];
 
+let display_valueA = "";
+let display_valueB = "";
+let operation = null;
 
 // Dynamic creation of calculator cells
 let l = grid_values.length;
 for(let i = 0; i < l; i++){
-    let el = document.createElement("div");
+    let el = document.createElement("button");
+    if (i==0) el.disabled = true; // disable display interactivity
     el.innerHTML = grid_values[i];
     el.classList.add(grid_classes[i]);
     el.style.gridArea = grid_classes[i];
