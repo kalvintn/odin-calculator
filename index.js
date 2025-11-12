c91fa54fa223b498cb35b3bcc89d014d0986f239
kalvintn <63025530+kalvintn@users.noreply.github.com>
2025-11-11 19:54:03 -0500

    Functionality for operators, equals, and clear.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let calculator = document.querySelector(".calculator");
-let grid_values = ["0", "&#x2b;", "&#8722;", "&#215;", "&#247;", "7", "8", "9", "AC", "4", "5", "6", "&#8592;", "1", "2", "3", "=", "0", "."];
+let grid_values = ["", "&#x2b;", "&#8722;", "&#215;", "&#247;", "7", "8", "9", "AC", "4", "5", "6", "&#8592;", "1", "2", "3", "=", "0", "."];
 let grid_classes = ["display", "add", "subtract", "multiply", "divide", "seven", "eight", "nine", "clear", "four", "five", "six", "backspace", "one", "two", "three", "equals", "zero", "decimal"];
 
 let display_valueA = "";
@@ -7,21 +7,85 @@ let display_valueB = "";
 let operation = null;
 
 // Dynamic creation of calculator cells
-let l = grid_values.length;
-for(let i = 0; i < l; i++){
+let grid_length = grid_values.length;
+for(let i = 0; i < grid_length; i++){
     let el = document.createElement("button");
     if (i==0) el.disabled = true; // disable display interactivity
     el.innerHTML = grid_values[i];
     el.classList.add(grid_classes[i]);
     el.style.gridArea = grid_classes[i];
+
+    // add event listener to buttons (nums and operands)
+    el.addEventListener("click", function () {
+        update(el);
+    });
+
     calculator.appendChild(el);
 }
 
 
 
 
-operation = "";
-console.log(operate(10,2));
+// Update function for display, values
+function update(button){
+    let display = document.querySelector(".display");
+
+    // Controls clear
+    if(button.innerHTML == "AC"){
+        // reset global values
+        display_valueA = "";
+        display_valueB = "";
+        operation = null;
+
+        // clear display
+        display.innerHTML = "";
+    }
+
+    // Controls operators
+    if(button.classList.contains("add") || button.classList.contains("subtract") || button.classList.contains("multiply") || button.classList.contains("divide")){
+        // only apply operand if num1 entered, and num2 not entered
+        if(!(display_valueA == "") && display_valueB == ""){
+            operation = button.classList[0];
+        }
+    }
+
+    // Controls number entry
+    if(!(isNaN(Number(button.innerHTML)))){
+        // First number (no operation set)
+        if(operation == null){
+            display_valueA += button.innerHTML;
+            display.innerHTML += button.innerHTML;
+        }
+         // Second number (operation set)
+        else if(operation) {
+            // clear display
+            display.innerHTML = "";
+            
+            display_valueB += button.innerHTML;
+
+            display.innerHTML += display_valueB;
+
+            console.log(display_valueA + " " + display_valueB);
+        }
+    }
+
+    // Controls equals
+    if(button.classList.contains("equals")){
+        // If numbers and operator defined
+        if(display_valueA && display_valueB && operation){
+            display.innerHTML = operate(display_valueA, display_valueB);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 
 // Uses global "operation" variable
 function operate(a, b){
@@ -55,3 +119,5 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+
